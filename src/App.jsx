b92b2c0c9fdf52a4b4af9851c9d8db52a8aff981
ayethandar <ayethandar@abank.com.mm>
2024-03-29 13:44:44 +0630

    add deploy yml file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { jsQuizz } from "./constants";
 import Quiz from "./Quiz";
 
 function App() {
-  return (
-    <>
-      <Quiz questions={jsQuizz.questions} />
-    </>
-  );
+  return <Quiz questions={jsQuizz.questions} />;
 }
 
 export default App;
